0b7b244fe47bfc8cc30d28461b3015683d0c85f9
brodie02 <marshallbrodie0@gmail.com>
2022-11-13 10:39:46 +1030

    Finish index.js

--- index.js
+++ index.js
@@ -68,3 +68,30 @@ inquirer.prompt([
         ]
     } 
 ])
+.then((data) => {
+    const manager = new Manager(data.name, data.id, data.email, data.officeNum)
+    const employees = data.employees
+    const engineers = []
+    const interns = []
+
+    for(let i = 0; i < employees.length; i++) {
+        const e = employees[i]
+        if (e.type === "Engineer") {
+            const engineer = new Engineer(e.name, e.id, e.email, e.github)
+            engineers.push(engineer)
+        } else if (e.type === "Intern") {
+            const intern = new Intern(e.name, e.id, e.email, e.school)
+            interns.push(intern)
+        }
+    }
+
+    const managerCard = createManagerCard(manager)
+    const engineerCards = createEngineerCards(engineers)
+    const internCards = createInternCards(interns)
+    const html = createHtml(managerCard, engineerCards, internCards)
+
+
+    fs.writeFile('index.html', html, (err) =>
+        err ? console.error(err) : console.log('Success!')
+    );
+})
